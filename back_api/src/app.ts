3846b0bc4827cd75dd497292ddea779a78a9c6bf
Richard Gama <richardgama@yahoo.com.mx>
2021-04-12 23:31:49 -0500

    config app morgan, cors, json

--- back_api/src/app.ts
+++ back_api/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan'
+import cors from 'cors'
 import config from './config';
 import dealerRoutes from './routes/dealer.routes';
 
@@ -6,6 +8,10 @@ const app = express();
 
 app.set('port', config.PORT );
 
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(dealerRoutes);
 
 export default app;
